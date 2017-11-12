3bf5bf7271889e967d555cd822acdda4083798bf
DimChtz <33261325+DimChtz@users.noreply.github.com>
2017-11-12 18:25:35 +0200

    Add files via upload

---
+++ brainwords.rb
@@ -0,0 +1,38 @@
+def generateBFCode(text)
+
+    bf = ""
+    lastchar = 0
+
+    text.each_byte { |c|
+
+        factor = 0
+        remain = 0
+        direction = '+'
+
+        if c > lastchar
+            factor = (Math.sqrt(c - lastchar)).floor
+            remain = (c - lastchar) - (factor * factor)
+            direction = '+'
+        else
+            factor = (Math.sqrt(lastchar - c)).floor
+            remain = (lastchar - c) - (factor * factor)
+            direction = '-'
+        end
+
+        bf += ('+' * factor) + '[>' + (direction * factor) + '<-]>' + (direction * remain) + '.<'
+
+        lastchar = c
+
+    }
+
+    return bf
+
+end
+
+
+
+
+#s = 'ABCDEFG'
+#for pos in 0...s.length
+#    puts s[pos].chr
+#end
